cc567fd238e2218e44fbcb667ba9f0a9139c66d5
Jack Bonaguro <jbonaguro2@gmail.com>
2018-10-11 18:38:25 -0400

    Initial commit, works but no state

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+  fs.readFile('./public/index.html', (err, data) => {
+    res.send(data);
+  });
+});*/
+
+module.exports = router;
